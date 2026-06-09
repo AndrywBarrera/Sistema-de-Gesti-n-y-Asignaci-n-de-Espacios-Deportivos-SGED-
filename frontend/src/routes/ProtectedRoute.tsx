ad4ef3859f38498d3d377eedc2a84bebfe194003
AndrywBarrera <167140102+AndrywBarrera@users.noreply.github.com>
2026-06-08 20:53:16 -0500

    Delete frontend directory
    Viejo Frond Delete

--- frontend/src/routes/ProtectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { getAuthToken } from "../api/axios";
-
-export default function ProtectedRoute() {
-  const isAuth = !!getAuthToken();
-  return isAuth ? <Outlet /> : <Navigate to="/" replace />;
-}
